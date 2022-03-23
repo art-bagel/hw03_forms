a40ec47ae5d5c535928e36ee2fb39f88e06e5b45
art-bagel <mr.bagel@yandex.ru>
2022-03-23 15:48:37 +0300

    Первая итерация

--- yatube/core/context_processors/year.py
+++ yatube/core/context_processors/year.py
@@ -5,7 +5,7 @@ from django.http import HttpRequest
 
 
 def year(request: HttpRequest) -> Dict[str, int]:
-    """Возвращаем словарь с текущим годом."""
+    """Возвращает словарь с текущим годом."""
     return {
         'year': datetime.now().year
     }
--- yatube/core/templatetags/user_filters.py
+++ yatube/core/templatetags/user_filters.py
@@ -1,8 +1,10 @@
 from django import template
+from django.forms import BoundField
 
 register = template.Library()
 
 
 @register.filter
-def addclass(field, css):
+def addclass(field: BoundField, css: str) -> str:
+    """Добавляет атрибут class в тег."""
     return field.as_widget(attrs={'class': css})
